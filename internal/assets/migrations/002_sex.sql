cb2f0c89e122e4fc0a144e380e9cdb6bd77e3ccc
chabanyknikita <nikchabanyk@gmail.com>
2024-09-17 12:05:05 +0300

    fix migrations and delete callback-url from links

--- internal/assets/migrations/002_sex.sql
+++ internal/assets/migrations/002_sex.sql
@@ -1,4 +1,4 @@
 -- +migrate Up
-ALTER TABLE verify_users ADD COLUMN sex BOOLEAN;
+ALTER TABLE verify_users ADD COLUMN sex BOOLEAN NOT NULL DEFAULT FALSE;
 -- +migrate Down
 ALTER TABLE verify_users DROP COLUMN sex;
--- resources/model_links_attributes.go
+++ resources/model_links_attributes.go
@@ -5,8 +5,6 @@
 package resources
 
 type LinksAttributes struct {
-	// It is not returned by default, this link are in get_proof_params
-	CallbackUrl *string `json:"callback_url,omitempty"`
 	// Returns proof-parameters and callback_url
 	GetProofParams string `json:"get_proof_params"`
 }
