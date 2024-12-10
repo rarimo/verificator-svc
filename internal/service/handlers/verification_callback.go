e8e413e522174108043bf1aef5de6ab5571f3f12
Maksym Hrynenko <mg06062003@gmail.com>
2024-12-10 16:05:13 +0200

    add: extra logs to debug

--- internal/service/handlers/verification_callback.go
+++ internal/service/handlers/verification_callback.go
@@ -93,7 +93,11 @@ func VerificationCallback(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	if verifiedUser == nil {
-		Log(r).Error("user not found or eventData != userHashID")
+		Log(r).WithFields(logan.F{
+			"event_data":   getter.Get(zk.EventData),
+			"user_id_hash": userIDHash,
+			"id":           req.Data.ID,
+		}).Error("user not found or eventData != userHashID")
 		ape.RenderErr(w, problems.NotFound())
 		return
 	}
