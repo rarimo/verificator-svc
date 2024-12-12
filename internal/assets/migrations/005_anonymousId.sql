6558147023052b11094d72efa6ddac468c87f58f
Maksym Hrynenko <mg06062003@gmail.com>
2024-12-12 16:14:02 +0200

    fix (migrations): down actions order

--- internal/assets/migrations/005_anonymousId.sql
+++ internal/assets/migrations/005_anonymousId.sql
@@ -5,8 +5,8 @@ ALTER TABLE verify_users ADD COLUMN nullifier TEXT DEFAULT '';
 CREATE UNIQUE INDEX verify_users_anonymous_id_unique ON verify_users(anonymous_id) WHERE anonymous_id != '';
 CREATE UNIQUE INDEX verify_users_nullifier_unique ON verify_users(nullifier) WHERE nullifier != '';
 -- +migrate Down
+DROP INDEX verify_users_nullifier_unique;
+DROP INDEX verify_users_anonymous_id_unique;
+
 ALTER TABLE verify_users DROP COLUMN anonymous_id;
 ALTER TABLE verify_users DROP COLUMN nullifier;
-
-DROP INDEX verify_users_anonymous_id_unique;
-DROP INDEX verify_users_nullifier_unique;
