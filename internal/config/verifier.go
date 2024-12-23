d7c2bf2997e0ffe90d3c00728f44dd5163925c18
Maksym Hrynenko <mg06062003@gmail.com>
2024-12-23 17:44:20 +0200

    add: erc1155 contract in config

--- internal/config/verifier.go
+++ internal/config/verifier.go
@@ -3,10 +3,13 @@ package config
 import (
 	"fmt"
 
+	"github.com/ethereum/go-ethereum/common"
 	zk "github.com/rarimo/zkverifier-kit"
 	"gitlab.com/distributed_lab/figure/v3"
 	"gitlab.com/distributed_lab/kit/kv"
 )
+
+const emptyETHAddr = "0x0000000000000000000000000000000000000000"
 
 type Verifiers struct {
 	Passport              *zk.Verifier
@@ -14,20 +17,25 @@ type Verifiers struct {
 	EventID               string
 	Multiproof            bool
 	PreserveUserIDCase    bool
+	ERC1155               common.Address
 }
 
 func (c *config) Verifiers() Verifiers {
 	return c.verifier.Do(func() interface{} {
-		var cfg struct {
-			VerificationKeyPath      string `fig:"verification_key_path,required"`
-			AllowedIdentityTimestamp int64  `fig:"allowed_identity_timestamp,required"`
-			EventID                  string `fig:"event_id,required"`
-			Multiproof               bool   `fig:"multiproof"`
-			PreserveUserIDCase       bool   `fig:"preserve_user_id_case"`
+		var cfg = struct {
+			VerificationKeyPath      string         `fig:"verification_key_path,required"`
+			AllowedIdentityTimestamp int64          `fig:"allowed_identity_timestamp,required"`
+			EventID                  string         `fig:"event_id,required"`
+			Multiproof               bool           `fig:"multiproof"`
+			PreserveUserIDCase       bool           `fig:"preserve_user_id_case"`
+			ERC1155                  common.Address `fig:"erc_1155"`
+		}{
+			ERC1155: common.HexToAddress(emptyETHAddr),
 		}
 
 		err := figure.
 			Out(&cfg).
+			With(figure.EthereumHooks).
 			From(kv.MustGetStringMap(c.getter, "verifier")).
 			Please()
 		if err != nil {
@@ -49,6 +57,7 @@ func (c *config) Verifiers() Verifiers {
 			EventID:               cfg.EventID,
 			Multiproof:            cfg.Multiproof,
 			PreserveUserIDCase:    cfg.PreserveUserIDCase,
+			ERC1155:               cfg.ERC1155,
 		}
 	}).(Verifiers)
 }
